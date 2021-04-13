57ad9ed58b914e5c03fe500deb757b846be7b204
meetpandya27 <78332547+meetpandya27@users.noreply.github.com>
2021-04-12 23:00:29 -0400

    Files updated

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from volume import *
 import unittest
 
-class TestCuboid (unittest.TestCase):
+class Test (unittest.TestCase):
     def test_volume(self):
-        self.assertAlmostEqual(volume(2),8)
-        self.assertAlmostEqual(volume(1),1)
         self.assertAlmostEqual(volume(0),0)
-        self.assertAlmostEqual(volume(5.5),166.375)
+        self.assertAlmostEqual(volume(1),1)
+        self.assertAlmostEqual(volume(3),27)
+        self.assertAlmostEqual(volume(10),1000)
 
 
 if __name__ == '__main__':
